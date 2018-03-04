1d20d29b80005aeea0d30c7839e0dc65a858a840
Sattar Hummatli <settarxan@gmail.com>
2018-03-04 12:49:47 +0400

    try constants in perl

---
+++ samples/constants.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+# hello.pl by Bill Weinman <http://bw.org/contact/>
+
+use 5.18.0;
+use warnings;
+
+use constant PI => 3.141592653589793238462643383279;
+sub PI2 { 3.141592653589793238462643383279 } #yuxaidaki ile eynidir, pragma bu cevrilir
+use constant TRUE => 1;
+use constant FALSE => '';
+
+#constantlar birlikde de define olune biler
+use constant {
+  X_C => 1,
+  CONST => "new str",
+  OTHER => "",
+};
+
+say PI;
+
+if (TRUE) {
+    say 'true';
+} else {
+    say 'false';
+}
+
